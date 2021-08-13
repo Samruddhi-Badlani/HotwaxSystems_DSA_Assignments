19944a941d9db3d8e39239aaff34d9d50656c11c
Samruddhi-Badlani <samruddhi4227132@gmail.com>
2021-08-13 14:07:22 +0530

    Added a TreeNode class:
    TreeNode contains :
    1. key : value
    2. left : left pointer to other node
    3. right : right pointer to other node

--- DS_Assignment1/src/LinkedList.java
+++ DS_Assignment1/src/LinkedList.java
@@ -1,15 +1,4 @@
-class Node{
-	int item;
-	Node next;
-	
-	public Node(int item) {
-		
-		// TODO Auto-generated constructor stub
-		this.item = item;
-		this.next = null;
-		
-	}
-}
+
 public class LinkedList {
 	
 	Node headNode;
---
+++ DS_Assignment1/src/Node.java
@@ -0,0 +1,12 @@
+class Node{
+	int item;
+	Node next;
+	
+	public Node(int item) {
+		
+		// TODO Auto-generated constructor stub
+		this.item = item;
+		this.next = null;
+		
+	}
+}
