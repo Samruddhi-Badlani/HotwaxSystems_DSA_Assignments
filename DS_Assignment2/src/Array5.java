1ccb73b6c88b79e548739196fff19455bad4793c
Samruddhi-Badlani <samruddhi4227132@gmail.com>
2021-08-18 16:48:19 +0530

    Solution for: Print all triplets that form an arithmetic progression Given a sorted array of distinct positive integers, print all triplets that forms an arithmetic progression with an integral common difference.
    Algorithm used :
    1. We consider every element as a middle element of AP.
    2. We find first and third term of AP in left and right of current
    element

---
+++ DS_Assignment2/src/Array5.java
@@ -0,0 +1,43 @@
+
+public class Array5 {
+	 public static void findAllTriplets(int[] A)
+	    {
+	        if (A.length < 3) {
+	            return;
+	        }
+	 
+	        
+	        for (int j = 1; j < A.length - 1; j++)
+	        {
+	           
+	            int i = j - 1, k = j + 1;
+	 
+	            while (i >= 0 && k < A.length)
+	            {
+	              
+	                if (A[i] + A[k] == 2 * A[j])
+	                {
+	                   
+	                    System.out.println(A[i] + " " + A[j] + " " + A[k]);
+	 
+	                    
+	                    k++; i--;
+	                }
+	            
+	                else if (A[i] + A[k] < 2 * A[j]) {
+	                    k++;
+	                }
+	                else {
+	                    i--;
+	                }
+	            }
+	        }
+	    }
+	 
+	    public static void main(String[] args)
+	    {
+	        int[] A = { 1, 3, 5, 6, 8, 9, 15 };
+	 
+	        findAllTriplets(A);
+	    }
+}
